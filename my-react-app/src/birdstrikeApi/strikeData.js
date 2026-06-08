ba3d3f181d8d988ae70ec726937d1e7ad13f11ee
Baks <trawallybakary7@gmail.com>
2026-06-08 01:39:15 +0000

    changed url

--- my-react-app/src/birdstrikeApi/strikeData.js
+++ my-react-app/src/birdstrikeApi/strikeData.js
@@ -1,8 +1,26 @@
+// import axios from "axios";
+
+// export default axios.create({
+// <<<<<<< HEAD
+//   baseURL: "https://appairside-frontend.onrender.com/api",
+//   headers: { "Content-Type": "application/json" },
+// });
+
+
+// =======
+//   baseURL: "http://localhost:3000/api",
+//   headers: { "Content-Type": "application/json" },
+// });
+// >>>>>>> f4d5fd8 (changed url)
+
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://appairside-frontend.onrender.com/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://appairside-frontend.onrender.com/api"
+      : "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-
