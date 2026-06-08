5f0a3edc01a5dbd9985b1346871c6c60e80d2050
Baks <trawallybakary7@gmail.com>
2026-06-08 02:28:42 +0000

    changed

--- my-react-app/src/birdstrikeApi/strikeData.js
+++ my-react-app/src/birdstrikeApi/strikeData.js
@@ -1,18 +1,3 @@
-// import axios from "axios";
-
-// export default axios.create({
-// <<<<<<< HEAD
-//   baseURL: "https://appairside-frontend.onrender.com/api",
-//   headers: { "Content-Type": "application/json" },
-// });
-
-
-// =======
-//   baseURL: "http://localhost:3000/api",
-//   headers: { "Content-Type": "application/json" },
-// });
-// >>>>>>> f4d5fd8 (changed url)
-
 import axios from "axios";
 
 export default axios.create({
