0699fae3cce1503205c0a0891e4d1b5dccdb9b5d
Baks <trawallybakary7@gmail.com>
2026-06-09 02:46:20 +0000

    changed strings cloud

--- my-react-app/src/birdstrikeApi/strikeData.js
+++ my-react-app/src/birdstrikeApi/strikeData.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://appairside-frontend.onrender.com/api"
-      : "http://localhost:3000/api",
+  baseURL:"http://localhost:4000/app",
+  // baseURL:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://appairside-frontend.onrender.com/api"
+  //     : "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+
+import axios from "axios";
+export default axios.create({
+    // baseURL:"http://localhost:3001"
+    baseURL:"http://localhost:4000/app"  
+})
--- my-react-app/src/pages/Register.jsx
+++ my-react-app/src/pages/Register.jsx
@@ -10,18 +10,36 @@ const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 
 
- const submit= (e) =>{
-    e.preventDefault();
-     api.post("/register", { name, email, password })
-    .then(user => {
-    // Save user to localStorage
-      console.log(user.data)
-      localStorage.setItem("user", JSON.stringify(user.data));
-    })
-    .catch(error => console.log(error))
-    navigate('/')
- }
+//  const submit= (e) =>{
+//     e.preventDefault();
+//      api.post("/register", { name, email, password })
+//     .then(user => {
+//     // Save user to localStorage
+//       console.log(user.data)
+//       localStorage.setItem("user", JSON.stringify(user.data));
+//     })
+//     .catch(error => console.log(error))
+//     navigate('/')
+//  }
+const submit = async (e) => {
+  e.preventDefault();
 
+  try {
+    const user = await api.post("/register", {
+      name,
+      email,
+      password,
+    });
+
+    console.log(user.data);
+
+    localStorage.setItem("user", JSON.stringify(user.data));
+
+    navigate("/");
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+  }
+};
   return (
     <div className='d-flex vh-98  mt-3 justify-content-center align-items-center'>
     <div className=' bg-whi w-[1500px] text-black rounded p-3'>  
