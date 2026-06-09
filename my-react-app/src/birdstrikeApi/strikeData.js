f2445de74a3d6395d48217c949e60337500b73ac
Baks <trawallybakary7@gmail.com>
2026-06-09 03:05:38 +0000

    changed strings cloud

--- my-react-app/src/birdstrikeApi/strikeData.js
+++ my-react-app/src/birdstrikeApi/strikeData.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:"https://appairside-frontend.onrender.com/api",
-  // baseURL:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://appairside-frontend.onrender.com/api"
-  //     : "http://localhost:3000/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://appairside-frontend.onrender.com/api"
+      : "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
