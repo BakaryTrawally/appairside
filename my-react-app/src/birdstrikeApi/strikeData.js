6189a5eb0828fc7dced370ed1268acaf261b3eec
Baks <trawallybakary7@gmail.com>
2026-06-10 00:30:34 +0000

    adding backend url

--- my-react-app/src/birdstrikeApi/strikeData.js
+++ my-react-app/src/birdstrikeApi/strikeData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://appairside-1.onrender.com/api"
+      ? "https://appairside.onrender.com/api"
       : "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
