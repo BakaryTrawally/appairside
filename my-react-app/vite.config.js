9927bbb7950ef708ebe7900d9a3e22d19012d240
Baks <trawallybakary7@gmail.com>
2026-06-14 15:21:13 +0000

    add another file

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ],
-})
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Bird Strike Reporting System",
+        short_name: "BirdStrike",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
